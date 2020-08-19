13fc51fd07240b41eb4495d9306cf9f96707b700
Thomas Fussell <thomas@giantswarm.io>
2020-08-19 03:42:29 -0400

    Miscellaneous improvements (#17)
    * add logging
    
    * add kubectl
    
    * add cluster id output
    
    * add wait flag
    
    * use context
    
    * improve logging
    
    * increase wait timeout

--- cmd/create/flag.go
+++ cmd/create/flag.go
@@ -11,47 +11,56 @@ import (
 )
 
 const (
-	defaultKubeconfigPath = "/workspace/cluster/kubeconfig"
+	defaultOutputClusterIDPath  = "/workspace/cluster/cluster-id"
+	defaultOutputKubeconfigPath = "/workspace/cluster/kubeconfig"
 
-	flagKubeconfig = "kubeconfig"
-	flagEndpoint   = "endpoint"
-	flagInCluster  = "in-cluster"
-	flagOutput     = "output"
-	flagProvider   = "provider"
-	flagRelease    = "release"
-	flagReleases   = "releases"
-	flagToken      = "token"
+	flagKubeconfig       = "kubeconfig"
+	flagEndpoint         = "endpoint"
+	flagInCluster        = "in-cluster"
+	flagOutputClusterID  = "output-cluster-id"
+	flagOutputKubeconfig = "output-kubeconfig"
+	flagProvider         = "provider"
+	flagRelease          = "release"
+	flagReleases         = "releases"
+	flagToken            = "token"
+	flagWait             = "wait"
 )
 
 type flag struct {
-	Kubeconfig string
-	Endpoint   string
-	InCluster  bool
-	Provider   string
-	OutputPath string
-	Release    string
-	Releases   string
-	Token      string
+	Kubeconfig       string
+	Endpoint         string
+	InCluster        bool
+	Provider         string
+	OutputClusterID  string
+	OutputKubeconfig string
+	Release          string
+	Releases         string
+	Token            string
+	Wait             bool
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&f.Kubeconfig, flagKubeconfig, "k", "", `The path to the kubeconfig for the control plane.`)
 	cmd.Flags().StringVarP(&f.Endpoint, flagEndpoint, "n", "", `The endpoint of the target control plane's API.`)
 	cmd.Flags().BoolVarP(&f.InCluster, flagInCluster, "i", false, `True if this program is running in a Kubernetes cluster and should communicate with the API via the injected service account token.`)
-	cmd.Flags().StringVarP(&f.OutputPath, flagOutput, "o", "", fmt.Sprintf(`The path for storing the kubeconfig for the created cluster.`))
+	cmd.Flags().StringVar(&f.OutputClusterID, flagOutputClusterID, "", fmt.Sprintf(`The path of the file in which to store the cluster ID of the created cluster.`))
+	cmd.Flags().StringVar(&f.OutputKubeconfig, flagOutputKubeconfig, "", fmt.Sprintf(`The path of the file in which to store the kubeconfig of the created cluster.`))
 	cmd.Flags().StringVarP(&f.Provider, flagProvider, "p", "", fmt.Sprintf(`The provider of the target release. Possible values: <%s>`, strings.Join(gsclient.AllProviders(), "|")))
 	cmd.Flags().StringVarP(&f.Release, flagRelease, "r", "", fmt.Sprintf(`The semantic version of the release to be tested.`))
 	cmd.Flags().StringVarP(&f.Releases, flagReleases, "s", "", fmt.Sprintf(`The path of the releases repo on the local filesystem.`))
 	cmd.Flags().StringVarP(&f.Token, flagToken, "t", "", `The token used to authenticate with the GS API.`)
+	cmd.Flags().BoolVarP(&f.Wait, flagWait, "w", false, `If true, wait for the test cluster API to be accessible before exiting.`)
 }
 
 func (f *flag) Validate() error {
 	if f.Kubeconfig == "" && !f.InCluster || f.Kubeconfig != "" && f.InCluster {
 		return microerror.Maskf(invalidFlagError, "--%s and --%s are mutually exclusive", flagKubeconfig, flagInCluster)
 	}
-	if f.OutputPath == "" {
-		// Set default output path if none is given
-		f.OutputPath = defaultKubeconfigPath
+	if f.OutputClusterID == "" {
+		f.OutputClusterID = defaultOutputClusterIDPath
+	}
+	if f.OutputKubeconfig == "" {
+		f.OutputKubeconfig = defaultOutputKubeconfigPath
 	}
 	if f.Releases == "" {
 		return microerror.Maskf(invalidFlagError, "--%s is required", flagReleases)
