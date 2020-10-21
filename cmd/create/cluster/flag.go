e12e2bfa2026fb84f24244402e97d70b4f01771f
Bricktop <marcel.mueller1@rwth-aachen.de>
2020-10-21 13:36:16 +0200

    Restructure commands to make release creation independent (#46)
    * Restructure commands to make release creation independent
    
    * Move provider logic to correct command
    
    * Add changelog

---
+++ cmd/create/cluster/flag.go
@@ -0,0 +1,56 @@
+package cluster
+
+import (
+	"strings"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/giantswarm/microerror"
+	"github.com/spf13/cobra"
+)
+
+const (
+	flagConfig     = "config"
+	flagKubeconfig = "kubeconfig"
+	flagOutput     = "output"
+	flagProvider   = "provider"
+	flagRelease    = "release"
+)
+
+type flag struct {
+	Config     string
+	Kubeconfig string
+	Provider   string
+	Output     string
+	Release    string
+}
+
+func (f *flag) Init(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(&f.Config, flagConfig, "g", "", `The path to the file containing API endpoints and tokens for each provider.`)
+	cmd.Flags().StringVarP(&f.Kubeconfig, flagKubeconfig, "k", "", `The path to the directory containing the kubeconfig for provider control planes.`)
+	cmd.Flags().StringVar(&f.Output, flagOutput, "", `The directory in which to store the cluster ID, kubeconfig, and provider of the created cluster.`)
+	cmd.Flags().StringVarP(&f.Provider, flagProvider, "p", "", `The provider of the target control plane.`)
+	cmd.Flags().StringVarP(&f.Release, flagRelease, "r", "", `The semantic version of the release to be tested.`)
+}
+
+func (f *flag) Validate() error {
+	if f.Config == "" {
+		return microerror.Maskf(invalidFlagError, "--%s is required", flagConfig)
+	}
+	if f.Kubeconfig == "" {
+		return microerror.Maskf(invalidFlagError, "--%s is required", flagKubeconfig)
+	}
+	if f.Output == "" {
+		return microerror.Maskf(invalidFlagError, "--%s is required", flagOutput)
+	}
+	if f.Release != "" {
+		f.Release = strings.TrimPrefix(f.Release, "v")
+		if _, err := semver.NewVersion(f.Release); err != nil {
+			return microerror.Maskf(invalidFlagError, "--%s must be a valid semantic version", flagRelease)
+		}
+		if f.Provider == "" {
+			return microerror.Maskf(invalidFlagError, "--%s must be specified when defining an exact release version", flagRelease)
+		}
+	}
+
+	return nil
+}
---
+++ cmd/create/cluster/runner.go
@@ -0,0 +1,119 @@
+package cluster
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+	"time"
+
+	"github.com/giantswarm/backoff"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/standup/pkg/config"
+	"github.com/giantswarm/standup/pkg/gsclient"
+)
+
+type runner struct {
+	flag   *flag
+	logger micrologger.Logger
+	stdout io.Writer
+	stderr io.Writer
+}
+
+func (r *runner) Run(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+
+	err := r.flag.Validate()
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.run(ctx, cmd, args)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (r *runner) run(ctx context.Context, _ *cobra.Command, _ []string) error {
+	var providerConfig *config.ProviderConfig
+	{
+		var err error
+		providerConfig, err = config.LoadProviderConfig(r.flag.Config, r.flag.Provider)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	// Create a GS API client for managing tenant clusters
+	var gsClient *gsclient.Client
+	{
+		c := gsclient.Config{
+			Logger: r.logger,
+
+			Endpoint: providerConfig.Endpoint,
+			Username: providerConfig.Username,
+			Password: providerConfig.Password,
+			Token:    providerConfig.Token,
+		}
+
+		var err error
+		gsClient, err = gsclient.New(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	// Create the cluster under test
+	var clusterID string
+	r.logger.LogCtx(ctx, "message", "creating cluster using target release")
+	{
+		var err error
+		clusterID, err = gsClient.CreateCluster(ctx, r.flag.Release)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+	r.logger.LogCtx(ctx, "message", fmt.Sprintf("created cluster %s", clusterID))
+
+	// Write cluster ID to filesystem
+	{
+		clusterIDPath := filepath.Join(r.flag.Output, "cluster-id")
+		r.logger.LogCtx(ctx, "message", fmt.Sprintf("writing cluster ID to path %s", clusterIDPath))
+		err := ioutil.WriteFile(clusterIDPath, []byte(clusterID), 0644)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	kubeconfigPath := filepath.Join(r.flag.Output, "kubeconfig")
+	r.logger.LogCtx(ctx, "message", fmt.Sprintf("creating and writing kubeconfig for cluster %s to path %s", clusterID, kubeconfigPath))
+	{
+		o := func() error {
+			// Create a keypair and kubeconfig for the new tenant cluster
+			err := gsClient.CreateKubeconfig(ctx, clusterID, kubeconfigPath)
+			if err != nil {
+				// TODO: check to see if it's a permanent error or the kubeconfig just isn't ready yet
+				r.logger.LogCtx(ctx, "message", "error creating kubeconfig", "error", err)
+				return microerror.Mask(err)
+			}
+			return nil
+		}
+		// Retry basically forever, the tekton task will determine maximum runtime.
+		b := backoff.NewMaxRetries(^uint64(0), 20*time.Second)
+
+		err := backoff.Retry(o, b)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	r.logger.LogCtx(ctx, "message", "setup complete")
+
+	return nil
+}
