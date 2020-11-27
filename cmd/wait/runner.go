87ec1fc8b4bab2604dfaf112fce1b1fd3996c9f2
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2020-11-27 12:35:39 +0100

    Wait for external-dns in wait task

--- cmd/wait/runner.go
+++ cmd/wait/runner.go
@@ -199,5 +199,65 @@ func (r *runner) run(ctx context.Context, _ *cobra.Command, _ []string) error {
 		r.logger.LogCtx(ctx, "message", "CoreDNS is ready")
 	}
 
+	if r.flag.Provider != "KVM" {
+		r.logger.LogCtx(ctx, "message", "waiting for external-dns to be ready")
+
+		targetLabels := map[string]string{
+			"giantswarm.io/service-type": "managed",
+			"app.kubernetes.io/name":     "external-dns",
+		}
+
+		o := func() error {
+			serviceLabelSelector := labelsToSelector(targetLabels)
+			services, err := k8sClient.CoreV1().Services("kube-system").List(ctx, v1.ListOptions{
+				LabelSelector: serviceLabelSelector,
+			})
+			if err != nil {
+				r.logger.LogCtx(ctx, "message", "error listing services", "error", err)
+				return microerror.Mask(err)
+			}
+			if len(services.Items) == 0 {
+				message := fmt.Sprintf("external-dns service not found using label selector %#q", serviceLabelSelector)
+				r.logger.LogCtx(ctx, "message", message)
+				return microerror.Mask(notReadyError)
+			}
+
+			service := services.Items[0]
+			podLabelSelector := labelsToSelector(service.Spec.Selector)
+			pods, err := k8sClient.CoreV1().Pods("kube-system").List(ctx, v1.ListOptions{
+				LabelSelector: podLabelSelector,
+			})
+			if err != nil {
+				r.logger.LogCtx(ctx, "message", "error listing external-dns pods", "error", err)
+				return microerror.Mask(err)
+			}
+			if len(pods.Items) == 0 {
+				message := fmt.Sprintf("external-dns pods not found using label selector %#q", podLabelSelector)
+				r.logger.LogCtx(ctx, "message", message)
+				return microerror.Mask(notReadyError)
+			}
+
+			for _, pod := range pods.Items {
+				for _, container := range pod.Status.ContainerStatuses {
+					if !container.Ready {
+						message := fmt.Sprintf("external-dns pod container %#q not ready", container.Name)
+						r.logger.LogCtx(ctx, "message", message)
+						return microerror.Mask(notReadyError)
+					}
+				}
+			}
+
+			return nil
+		}
+		// Retry basically forever, the tekton task will determine maximum runtime.
+		b := backoff.NewMaxRetries(^uint64(0), 20*time.Second)
+
+		err = backoff.Retry(o, b)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		r.logger.LogCtx(ctx, "message", "external-dns is ready")
+	}
+
 	return nil
 }
