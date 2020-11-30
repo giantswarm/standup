ef1d4de6f1a2c4aa6b14efa4f3180afaa94c27ab
Bricktop <marcel.mueller1@rwth-aachen.de>
2020-11-30 15:37:54 +0100

    Wait for external-dns in wait task (#58)
    * Wait for external-dns in wait task
    
    * provider lowercase
    
    * Add comment
    
    * Add changelog

--- cmd/wait/flag.go
+++ cmd/wait/flag.go
@@ -7,19 +7,25 @@ import (
 
 const (
 	flagKubeconfig = "kubeconfig"
+	flagProvider   = "provider"
 )
 
 type flag struct {
 	Kubeconfig string
+	Provider   string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&f.Kubeconfig, flagKubeconfig, "k", "", `The path to the kubeconfig for the tenant cluster.`)
+	cmd.Flags().StringVarP(&f.Provider, flagProvider, "p", "", `The provider of the target control plane.`)
 }
 
 func (f *flag) Validate() error {
 	if f.Kubeconfig == "" {
 		return microerror.Maskf(invalidFlagError, "--%s is required", flagKubeconfig)
+	}
+	if f.Provider == "" {
+		return microerror.Maskf(invalidFlagError, "--%s is required", flagProvider)
 	}
 
 	return nil
