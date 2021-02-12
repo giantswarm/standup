d7dcf9d4ac249dafed8e84ba28c99ebb118bb180
GitHub Action <action@github.com>
2021-02-12 14:32:02 +0000

    Bump version to 2.6.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "standup"
 	source      = "https://github.com/giantswarm/standup"
-	version     = "2.6.0"
+	version     = "2.6.1-dev"
 )
 
 func Description() string {
