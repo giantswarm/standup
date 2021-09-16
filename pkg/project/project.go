5b6156a0a9e3a287c8bbf7243e110989fceb0f68
GitHub Action <action@github.com>
2021-09-16 07:35:22 +0000

    Bump version to 3.1.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "standup"
 	source      = "https://github.com/giantswarm/standup"
-	version     = "3.1.0"
+	version     = "3.1.1-dev"
 )
 
 func Description() string {
