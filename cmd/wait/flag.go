a9ff2fcfe6a129681de101206ddb23fb6bd45171
Christian Bianchi <whites11@gmail.com>
2021-09-16 15:18:32 +0200

    Add flag to specify the expected number of nodes to be ready in `wait ready` command.

--- cmd/wait/flag.go
+++ cmd/wait/flag.go
@@ -6,18 +6,22 @@ import (
 )
 
 const (
-	flagKubeconfig = "kubeconfig"
-	flagProvider   = "provider"
+	flagKubeconfig        = "kubeconfig"
+	flagProvider          = "provider"
+	flagDesiredNodesCount = "nodes"
 )
 
 type flag struct {
-	Kubeconfig string
-	Provider   string
+	Kubeconfig        string
+	Provider          string
+	DesiredNodesCount int
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&f.Kubeconfig, flagKubeconfig, "k", "", `The path to the kubeconfig for the tenant cluster.`)
 	cmd.Flags().StringVarP(&f.Provider, flagProvider, "p", "", `The provider of the target control plane.`)
+	cmd.Flags().IntVarP(&f.DesiredNodesCount, flagDesiredNodesCount, "", 2, `The number of nodes to wait for.`)
+
 }
 
 func (f *flag) Validate() error {
@@ -26,6 +30,9 @@ func (f *flag) Validate() error {
 	}
 	if f.Provider == "" {
 		return microerror.Maskf(invalidFlagError, "--%s is required", flagProvider)
+	}
+	if f.DesiredNodesCount < 2 {
+		return microerror.Maskf(invalidFlagError, "--%s has to be bigger than 1", flagDesiredNodesCount)
 	}
 
 	return nil
